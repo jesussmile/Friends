bcfcb79b1b09c734dbc98ed97554d37af87188fa
jesussmile <pullbackwild@gmail.com>
2016-02-15 08:43:58 +0545

    Initial commit

---
+++ app/src/main/java/com/example/pannam/friends/FriendProvider.java
@@ -0,0 +1,7 @@
+package com.example.pannam.friends;
+
+/**
+ * Created by pannam on 2/15/2016.
+ */
+public class FriendProvider {
+}
---
+++ app/src/main/java/com/example/pannam/friends/FriendsDatabase.java
@@ -0,0 +1,13 @@
+package com.example.pannam.friends;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by pannam on 2/15/2016.
+ */
+//add SQLiteopenhelper is  a class that activates database
+    
+public class FriendsDatabase extends SQLiteOpenHelper{
+
+
+}
