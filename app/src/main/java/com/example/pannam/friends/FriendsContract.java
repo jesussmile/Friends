2b9857c7f9ec86e301e38de064c33098f58c5dd9
jesussmile <pullbackwild@gmail.com>
2016-02-15 10:19:10 +0545

    DATABASE CREATED

--- app/src/main/java/com/example/pannam/friends/FriendsContract.java
+++ app/src/main/java/com/example/pannam/friends/FriendsContract.java
@@ -1,7 +1,51 @@
 package com.example.pannam.friends;
 
+import android.net.Uri;
+import android.provider.BaseColumns;
+
 /**
  * Created by pannam on 2/15/2016.
  */
+//contains fixed data it basically helps provides information ,COLUMN NAME ETC ..BEST
+// PRACTICE TO SEPEARTE THIS
 public class FriendsContract {
+
+    //contains column friends table
+    interface FriendsColumns {
+        String FRIENDS_NAME = "friends_name";
+        String FRIENDS_EMAIL = "friends_email";
+        String FRIENDS_PHONE = "friends_phone";
+    }
+
+    public static final String CONTENT_AUTHORITY = "org.example.pannam.friends.provider";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    private static final String PATH_FRIENDS = "friends";
+    public static final String[] TOP_LEVEL_PATHS = {
+            PATH_FRIENDS
+    };
+
+    //basecolumn is from android itself
+    public static class Friends implements FriendsColumns, BaseColumns {
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendEncodedPath(PATH_FRIENDS).build();
+      //defining access more than one data (more than one friend)
+       //MIME TYPE multiple type
+        public static final String CONTENT_TYPE = "vnd.android.cursor.dir/vnd."+CONTENT_AUTHORITY + ".friends";
+       //single type
+        public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd."+CONTENT_AUTHORITY + ".friends";
+
+        //building Uri to access a particular record
+        public static Uri buildFriendUri(String friendId)
+        {
+
+            return CONTENT_URI.buildUpon().appendEncodedPath(friendId).build();
+
+        }
+        public static String getFriendUri(Uri uri){
+            return uri.getPathSegments().get(1);
+
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/pannam/friends/FriendsDatabase.java
+++ app/src/main/java/com/example/pannam/friends/FriendsDatabase.java
@@ -1,13 +1,72 @@
 package com.example.pannam.friends;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
 
 /**
  * Created by pannam on 2/15/2016.
  */
 //add SQLiteopenhelper is  a class that activates database
-    
-public class FriendsDatabase extends SQLiteOpenHelper{
 
+public class FriendsDatabase extends SQLiteOpenHelper {
+    //Logging
+    private static final String TAG = FriendsDatabase.class.getSimpleName();
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + Tables.FRIENDS + "{" + BaseColumns._ID
+                + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + FriendsContract.FriendsColumns.FRIENDS_NAME + "TEXT NOT NULL,"
+                + FriendsContract.FriendsColumns.FRIENDS_EMAIL + "TEXT NOT NULL,"
+                + FriendsContract.FriendsColumns.FRIENDS_PHONE + "TEXT NOT NULL)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        //INCASE THE USER UPGRADES THE APP ADD MORE FUNCTIONS ETC..
+        int version = oldVersion;
+        if (version == 1 ) {
+            //Add some extra fields to the database without deleting existing data
+
+            version =2 ;
+
+        }
+        if (version != DATABASE_VERSION) {
+            db.execSQL("DROP TABLE IF EXISTS "+ Tables.FRIENDS);
+            onCreate(db);
+        }
+
+    }
+
+    //friends.db is the data base that will get stored in android device
+    private static final String DATABASE_NAME = "friends.db";
+    private static final int DATABASE_VERSION = 2;
+    private final Context mContext;
+
+    interface Tables {
+        String FRIENDS = "friends";
+
+    }
+
+    //constructor
+
+
+    public FriendsDatabase(Context context) {
+        super(context, DATABASE_NAME, null
+                , DATABASE_VERSION);
+        mContext = context;
+    }
+
+
+    //delete database
+    public static void deleteDatabase (Context context){
+        context.deleteDatabase(DATABASE_NAME);
+
+    }
 
 }
+
+
