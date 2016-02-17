542e80d6eab56b11826262f7f9c15620f615eff2
jesussmile <pullbackwild@gmail.com>
2016-02-17 08:37:02 +0545

    DATABASE CREATED

--- app/src/main/java/com/example/pannam/friends/MainActivity.java
+++ app/src/main/java/com/example/pannam/friends/MainActivity.java
@@ -1,15 +1,11 @@
 package com.example.pannam.friends;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuInflater;
-import android.view.View;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class MainActivity extends FragmentActivity {
